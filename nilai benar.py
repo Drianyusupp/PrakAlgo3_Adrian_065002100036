0ef642fcfc536cc4c0a667fe24dd41bb4679fc2a
Drianyusupp <91178655+Drianyusupp@users.noreply.github.com>
2021-10-06 09:41:26 +0700

    Add files via upload

---
+++ nilai benar.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+10
+
+
+a = int(input("Masukkan angka awal: "))
+b = int(input("Masukkan angka akhir: "))
+
+while a<=b:
+    print(a,b)
+    a = a + 1
+    b = b - 1
+    
+    
